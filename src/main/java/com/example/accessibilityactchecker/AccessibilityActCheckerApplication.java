fc56fb971bc6f85336fb5e160815b269ab27aa08
Filip Stetter <filip.stetter@telia.com>
2024-11-18 12:39:22 +0100

    Create

---
+++ src/main/java/com/example/accessibilityactchecker/AccessibilityActCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.example.accessibilityactchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessibilityActCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessibilityActCheckerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/accessibilityactchecker/AccessibilityActCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessibilityactchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessibilityActCheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
